28c4c61d396996680433254a8aeebe4475f703ac
Don Patterson <djp3@ics.uci.edu>
2010-12-29 17:44:15 -0800

    Committing an initial stub program

---
+++ cacophony/src/edu/uci/ics/luci/cacophony/Cacophony.java
@@ -0,0 +1,9 @@
+package edu.uci.ics.luci.cacophony;
+
+public class Cacophony {
+	
+	public static void main(String[] args) {
+		System.out.println("Cacophony team members:");
+		System.out.println("	Donald J. Patterson");
+	}
+}
