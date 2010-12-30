eeb55dcfffff7e571b2a4a662a0490c3d1ac8c08
Don Patterson <djp3@ics.uci.edu>
2010-12-29 17:50:12 -0800

    Testing commit to production branch

--- cacophony/src/edu/uci/ics/luci/cacophony/Cacophony.java
+++ cacophony/src/edu/uci/ics/luci/cacophony/Cacophony.java
@@ -5,5 +5,6 @@ public class Cacophony {
 	public static void main(String[] args) {
 		System.out.println("Cacophony team members:");
 		System.out.println("	Donald J. Patterson");
+		System.out.println("	Add your name here");
 	}
 }
