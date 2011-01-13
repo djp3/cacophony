bda83829e4d3887f5f0e3be6e06a30e60c75db03
Don Patterson <djp3@ics.uci.edu>
2011-01-13 15:58:54 -0800

    concurrent change

--- cacophony/src/edu/uci/ics/luci/cacophony/Cacophony.java
+++ cacophony/src/edu/uci/ics/luci/cacophony/Cacophony.java
@@ -4,7 +4,7 @@ public class Cacophony {
 	
 	public static void main(String[] args) {
 		System.out.println("Cacophony team members:");
-		System.out.println("	Donald J. Patterson");
+		System.out.println("	Don J. Patterson");
 		System.out.println("	Raphael Chang Lee");
 		System.out.println("	Aaron Pecson" );
 	
