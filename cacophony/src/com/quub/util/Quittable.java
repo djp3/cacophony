11a94fa82916a5eca72eb0d28556e903d0745deb
Don Patterson <d_j_p_3@djp3.net>
2012-06-07 16:29:20 -0700

    Removing copied code from other projects in lieu of including the actual projects

--- cacophony/src/com/quub/util/Quittable.java
+++
@@ -1,9 +0,0 @@
-package com.quub.util;
-
-/* This is used for the shutdown manager */
-public interface Quittable {
-	
-	/** Consider making this synchronized*/
-	void setQuitting(boolean quitting);
-
-}
