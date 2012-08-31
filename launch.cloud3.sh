0ca4ca34a879ba881140b44d2970a01c54ccfd12
Don Patterson <d_j_p_3@djp3.net>
2012-08-31 16:36:11 -0700

    working on #87

--- launch.cloud3.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-java -Dfile.encoding=UTF-8 -Duser.timezone=GMT -Djava.net.preferIPv6Stack=true -jar directory.jar --url.external "cloud3.waitscout.com" --server.guid "cloud3" --port 1776 --config "cacophony/cacophony.properties"
-
