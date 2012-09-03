db5a87b52af837980b60ff0763335e30fbb0f76f
Don Patterson <d_j_p_3@djp3.net>
2012-09-03 10:09:01 -0700

    Adding revision file

--- cacophony/src/edu/uci/ics/luci/cacophony/GitRevision.java
+++ cacophony/src/edu/uci/ics/luci/cacophony/GitRevision.java
@@ -1,6 +1,5 @@
 package edu.uci.ics.luci.cacophony;
 
 public class GitRevision {
-	//Testing the commit hook.  If it's working then this line should disappear
-	public static final String SYSTEM_REVISION = "859965d87ee07f3e08563a4c41005ab4660b8ffe";
+	public static final String SYSTEM_REVISION = "a9175b89cc6f380c7c8bb42be15f7fe68e4d4aa1";
 }
