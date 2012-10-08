ab537ec9445f65dbd6d5a0f5b62cbd11ef05efd2
Don Patterson <d_j_p_3@djp3.net>
2012-10-07 23:18:47 -0700

    Ticket #130

--- cacophony/src/edu/uci/ics/luci/cacophony/GitRevision.java
+++ cacophony/src/edu/uci/ics/luci/cacophony/GitRevision.java
@@ -1,5 +1,5 @@
 package edu.uci.ics.luci.cacophony;
 
 public class GitRevision {
-	public static final String SYSTEM_REVISION = "1dad74cfcdf6e1cb07179dc69c1aefb6ccf48304";
+	public static final String SYSTEM_REVISION = "684550b00d2d1c2327ecb2b7afb8c5c8bb600cbd";
 }
