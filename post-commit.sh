784e03ca8d9252669a2600ba08295d48c8ae7294
Don Patterson <d_j_p_3@djp3.net>
2013-01-11 15:26:21 -0800

    Trying to get post-commit to work

--- post-commit.sh
+++ post-commit.sh
@@ -4,7 +4,7 @@ git stash -q --keep-index
 VERSION=`git log -1 --pretty=format:%H`
 #echo $VERSION
 
-cat > ./cacophony/src/edu/uci/ics/luci/cacophony/GitRevision.java << EOF
+cat > /Users/djp3/Development/Mac/EclipseWorkspaceCacophony/cacophony/src/edu/uci/ics/luci/cacophony/GitRevision.java << EOF
 package edu.uci.ics.luci.cacophony;
 
 public class GitRevision {
