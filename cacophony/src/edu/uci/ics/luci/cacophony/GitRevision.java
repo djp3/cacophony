55f2f157611eaf7e291e21cf4b187da84bd24181
Don Patterson <d_j_p_3@djp3.net>
2013-01-18 14:41:18 -0800

    Fixing "libs" -> "lib"

--- cacophony/src/edu/uci/ics/luci/cacophony/GitRevision.java
+++ cacophony/src/edu/uci/ics/luci/cacophony/GitRevision.java
@@ -1,5 +1,5 @@
 package edu.uci.ics.luci.cacophony;
 
 public class GitRevision {
-	public static final String SYSTEM_REVISION = "812e30be3606fdd6d04b640708114494a80ee11f";
+	public static final String SYSTEM_REVISION = "ce6e12d9461663700668d786aacd876d5a3cccaf";
 }
