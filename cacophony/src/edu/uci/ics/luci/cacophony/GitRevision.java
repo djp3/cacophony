29057ada23357e24cae2d91a1201da38fc48202a
Don Patterson <d_j_p_3@djp3.net>
2013-01-28 13:36:40 -0800

    Hopefuly that fixes it

--- cacophony/src/edu/uci/ics/luci/cacophony/GitRevision.java
+++ cacophony/src/edu/uci/ics/luci/cacophony/GitRevision.java
@@ -3,7 +3,11 @@ package edu.uci.ics.luci.cacophony;
 public class GitRevision {
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 	public static final String SYSTEM_REVISION = "5c1f81b30891996b74c178954d3318a2149aa1c6";
+=======
+	public static final String SYSTEM_REVISION = "55f2f157611eaf7e291e21cf4b187da84bd24181";
+>>>>>>> Stashed changes
 =======
 	public static final String SYSTEM_REVISION = "55f2f157611eaf7e291e21cf4b187da84bd24181";
 >>>>>>> Stashed changes
