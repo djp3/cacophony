53a2ed841749303528f0c2b35690e6c7cf406240
Don Patterson <d_j_p_3@djp3.net>
2013-01-28 13:17:53 -0800

    Rats nest

--- cacophony/src/edu/uci/ics/luci/cacophony/GitRevision.java
+++ cacophony/src/edu/uci/ics/luci/cacophony/GitRevision.java
@@ -1,5 +1,5 @@
 package edu.uci.ics.luci.cacophony;
 
 public class GitRevision {
-	public static final String SYSTEM_REVISION = "02080874945cd42e29c3ec725eaa5fd2d9a59c52";
+	public static final String SYSTEM_REVISION = "2a5e474f335c7406f7fe48b5e368638abb82a6e4";
 }
