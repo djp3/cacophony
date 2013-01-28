2a55fa7bcb1fbca8bc179cb06720b4f716996545
Don Patterson <d_j_p_3@djp3.net>
2013-01-28 13:29:45 -0800

    Cleaning up

--- cacophony/src/edu/uci/ics/luci/cacophony/GitRevision.java
+++ cacophony/src/edu/uci/ics/luci/cacophony/GitRevision.java
@@ -1,5 +1,5 @@
 package edu.uci.ics.luci.cacophony;
 
 public class GitRevision {
-	public static final String SYSTEM_REVISION = "a272886e04f0e764b247271944fce3d368fe7bb6";
+	public static final String SYSTEM_REVISION = "301791797244215ca49009d76872e2ef3801e544";
 }
