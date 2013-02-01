b2fbd7c6d11801a8aa97eb714a077d91ac0151ab
Don Patterson <d_j_p_3@djp3.net>
2013-02-01 14:03:49 -0800

    Updating cassandra configuration files

---
+++ external/cassandra_config/env.local.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+#Adjust the following 
+CACOPHONY_BASE=/Users/djp3/Development/Mac/EclipseWorkspaceCacophony
+LOCAL_HOSTNAME=localmac.djp3
+CASSANDRA_HOST_IP=128.195.59.254
+CASSANDRA_PORT=9160
+
+#Adjust the following paths, if necessary.  The default is the expected file structure
+
+CACOPHONY_TMP=$CACOPHONY_BASE/tmp
+CACOPHONY_EXTERNAL=$CACOPHONY_BASE/external
+
+CASSANDRA_BASE=$CACOPHONY_EXTERNAL/cassandra
+CASSANDRA_CONF=$CASSANDRA_BASE/conf
+
+CASSANDRA_CLI=$CASSANDRA_BASE/bin/cassandra-cli
+
+CASSANDRA_LOCAL_CONF=$CACOPHONY_EXTERNAL/cassandra_config
+
+#Adjust the following command locations if necessary
+ECHO=/bin/echo
+ID=/usr/bin/id
+
