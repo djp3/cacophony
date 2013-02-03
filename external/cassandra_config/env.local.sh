f73f7e437dcf0b6293341b8692b24fa71e1f940f
Don Patterson <d_j_p_3@djp3.net>
2013-02-02 17:07:15 -0800

    Final fixes for #147

--- external/cassandra_config/env.local.sh
+++ external/cassandra_config/env.local.sh
@@ -1,15 +1,19 @@
 #! /bin/bash
 
+###
 #Adjust the following 
 CACOPHONY_BASE=/Users/djp3/Development/Mac/EclipseWorkspaceCacophony
 LOCAL_HOSTNAME=localmac.djp3
 
-#Try and figure out ipaddress, if this doesn't work, then set it manually
-#CASSANDRA_HOST_IP=127.0.0.1
-CASSANDRA_HOST_IP=`ifconfig | grep "\<inet\>" | grep broadcast | cut -d" " -f2 | head -n1`
-CASSANDRA_PORT=9160
 
-#Adjust the following paths, if necessary.  The default is the expected file structure
+###
+#Adjust the following, if necessary.  The default is the expected file structure
+
+#Try and figure out ipaddress, if this doesn't work, then set it manually
+CASSANDRA_HOST_IP=`ifconfig | grep "\<inet\>" | grep broadcast | cut -d" " -f2 | head -n1`
+#CASSANDRA_HOST_IP=127.0.0.1
+
+CASSANDRA_PORT=9160
 
 CACOPHONY_TMP=$CACOPHONY_BASE/tmp
 CACOPHONY_EXTERNAL=$CACOPHONY_BASE/external
@@ -21,7 +25,9 @@ CASSANDRA_CLI=$CASSANDRA_BASE/bin/cassandra-cli
 
 CASSANDRA_LOCAL_CONF=$CACOPHONY_EXTERNAL/cassandra_config
 
+###
 #Adjust the following command locations if necessary
+
 ECHO=/bin/echo
 ID=/usr/bin/id
 
