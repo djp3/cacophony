93188ed4fe3b3096377cf1b73d886b832e0e1a15
Don Patterson <d_j_p_3@djp3.net>
2013-03-09 18:04:42 -0800

    Updating the git revisions #149

--- cacophony/src/edu/uci/ics/luci/cacophony/GitRevision.java
+++ cacophony/src/edu/uci/ics/luci/cacophony/GitRevision.java
@@ -1,5 +1,5 @@
 package edu.uci.ics.luci.cacophony;
 
 public class GitRevision {
-	public static final String SYSTEM_REVISION = "9f48fe5380c3a0e265439fcf934635810c60296a";
+	public static final String SYSTEM_REVISION = "22372d2ad452bd253923e443bf99340e2f2c44c2";
 }
--- examples/sensorLibrary/src/java/edu/uci/ics/luci/cacophony/sensors/GitRevision.java
+++ examples/sensorLibrary/src/java/edu/uci/ics/luci/cacophony/sensors/GitRevision.java
@@ -1,5 +1,5 @@
-package edu.uci.ics.luci.cacophony.sensors;
+package edu.uci.ics.luci.cacophony;
 
 public class GitRevision {
-	public static final String SYSTEM_REVISION = "bedbeef162cb8043a5c3d7b6a24a2b58b0b16c70";
+	public static final String SYSTEM_REVISION = "22372d2ad452bd253923e443bf99340e2f2c44c2";
 }
