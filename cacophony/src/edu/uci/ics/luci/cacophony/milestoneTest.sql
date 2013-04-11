c178705e9ccd0cd7a19e73be291aab60015a3b60
Don Patterson <d_j_p_3@djp3.net>
2013-04-11 15:27:34 -0700

    ticket #165

--- cacophony/src/edu/uci/ics/luci/cacophony/milestoneTest.sql
+++ cacophony/src/edu/uci/ics/luci/cacophony/milestoneTest.sql
@@ -5,9 +5,9 @@
 # http://www.sequelpro.com/
 # http://code.google.com/p/sequel-pro/
 #
-# Host: localhost (MySQL 5.5.29)
+# Host: localhost (MySQL 5.6.10)
 # Database: testDatabase
-# Generation Time: 2013-03-11 23:38:05 +0000
+# Generation Time: 2013-04-11 22:15:37 +0000
 # ************************************************************
 
 
@@ -28,10 +28,8 @@ DROP TABLE IF EXISTS `milestonetest`;
 CREATE TABLE `milestonetest` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(128) DEFAULT NULL,
-  `x` double DEFAULT NULL,
-  `y` double DEFAULT NULL,
-  `weight` double DEFAULT NULL,
   `point` point DEFAULT NULL,
+  `weight` double DEFAULT NULL,
   `configuration` varchar(1024) DEFAULT '{doTraining:false}',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -39,17 +37,17 @@ CREATE TABLE `milestonetest` (
 LOCK TABLES `milestonetest` WRITE;
 /*!40000 ALTER TABLE `milestonetest` DISABLE KEYS */;
 
-INSERT INTO `milestonetest` (`id`, `name`, `x`, `y`, `weight`, `point`, `configuration`)
+INSERT INTO `milestonetest` (`id`, `name`, `point`, `weight`, `configuration`)
 VALUES
-	(1,'Power Source',-117.897256,33.662671,10,X'000000000101000000F2086EA46C795DC0FA804067D2D44040','{doTraining:false,features:[],target:{URL:\"http://localhost:2011/sense?sensor=powersource&version=1.0&format=html\",XPath:\"/html/body/div\",regEx:\"(.*)\"}}'),
-	(2,'IP Address',-122.035977,37.323,10,X'000000000101000000209A79724D825EC0D34D621058A94240','{doTraining:false,features:[],target:{URL:\"http://localhost:2011/sense?sensor=ipaddress&version=1.0&format=html\",XPath:\"/html/body/div\",regEx:\"(.*)\"}}'),
-	(3,'WiFi SSID',-124.1475128,40.79905584,10,X'00000000010100000073EF86D970095FC0293D367647664440','{doTraining:false,features:[1,2],target:{URL:\"http://localhost:2011/sense?sensor=wifi&version=1.0&format=html\",XPath:\"/html/body/div\",regEx:\"<<(.*):[0-9a-f]*:[0-9a-f]*:[0-9a-f]*:[0-9a-f]*:[0-9a-f]*:[0-9a-f]*>:[-]?[0-9][0-9]*>\"\n}}'),
-	(4,'WIFI MAC',-118.1128944,33.75761197,10,X'000000000101000000B3F96EA939875DC0A41AD56DF9E04040','{doTraining:false,features:[],target:{URL:\"http://localhost:2011/sense?sensor=wifi&version=1.0&format=html\",XPath:\"/html/body/div\",regEx:\"<<.*:([0-9a-f]*:[0-9a-f]*:[0-9a-f]*:[0-9a-f]*:[0-9a-f]*:[0-9a-f]*)>:[-]?[0-9][0-9]*>\"\n}}'),
-	(5,'Volume',-122.0854,37.3812,10,X'00000000010100000005C58F3177855EC01B9E5E29CBB04240','{doTraining:false,features:[],target:{URL:\"http://localhost:2011/sense?sensor=volume&version=1.0&format=html\",XPath:\"/html/body/div\",regEx:\"(.*)\"}}'),
-	(6,'Light',-122.3021732,38.307215,10,X'00000000010100000092EE42CE56935EC09AEB34D252274340','{doTraining:false,features:[4,5],target:{URL:\"http://localhost:2011/sense?sensor=light&version=1.0&format=html\",XPath:\"/html/body/div\",regEx:\"(.*)\"}}'),
-	(7,'UI Activity',-122.1491087,37.42772227,10,X'000000000101000000248337FF8A895EC0DFB5749ABFB64240','{doTraining:false,features:[3,6],target:{URL:\"http://localhost:2011/sense?sensor=uiactivity&version=1.0&format=html\",XPath:\"/html/body/div\",regEx:\"(.*)\"}}'),
-	(8,'Idle Time',-117.07153,32.772907,10,X'000000000101000000B4AB90F293445DC0BBECD79DEE624040','{doTraining:false,features:[7],target:{URL:\"http://localhost:2011/sense?sensor=idle&version=1.0&format=html\",XPath:\"/html/body/div\",regEx:\"(.*)\"}}'),
-	(9,'Accelerometer Z',-122.4398944,37.7857039,10,X'000000000101000000636CD73A279C5EC07A6B05F291E44240','{doTraining:false,features:[6,8],target:{URL:\"http://localhost:2011/sense?sensor=accelerometer&version=1.0&format=html\",XPath:\"/html/body/div\",regEx:\",\\\\s*([0-9]*[.][0-9]*)[]]$\"}}');
+	(1,'power_source',X'000000000101000000F2086EA46C795DC0FA804067D2D44040',10,'{ \"do_training\": false, \"features\": [ { \"namespace\": null, \"names\": [ \"time\", \"weekend\" ] }, { \"namespace\": \"edu.uci.ics.luci.cacophony.milestone\", \"names\": [ \"wifi_mac\" ] } ], \"target\": { \"format\": \"html\", \"url\": \"http://localhost:2011/sense?sensor=powersource&version=1.0&format=html\", \"xpath\": \"/html/body/div\", \"regular_expression\": \"(.*)\", \"translator_class\": \"edu.uci.ics.luci.cacophony.node.TranslatorCategory\" } }\n\n\n'),
+	(2,'ip_address',X'000000000101000000209A79724D825EC0D34D621058A94240',10,'{ \"do_training\": false, \"features\": [ { \"namespace\": null, \"names\": [ \"time\", \"weekend\" ] }, { \"namespace\": \"edu.uci.ics.luci.cacophony.milestone\", \"names\": [ \"wifi_mac\" ] } ], \"target\": { \"format\": \"html\", \"url\": \"http://localhost:2011/sense?sensor=ipaddress&version=1.0&format=html\", \"xpath\": \"/html/body/div\", \"regular_expression\": \"(.*)\", \"translator_class\": \"edu.uci.ics.luci.cacophony.node.TranslatorCategory\" } }\n\n\n'),
+	(3,'wifi_ssid',X'00000000010100000073EF86D970095FC0293D367647664440',10,'{ \"do_training\": false, \"features\": [ { \"namespace\": null, \"names\": [ ] }, { \"namespace\": \"edu.uci.ics.luci.cacophony.milestone\", \"names\": [ \"wifi_mac\" ] } ], \"target\": { \"format\": \"html\", \"url\": \"http://localhost:2011/sense?sensor=wifi&version=1.0&format=html\", \"xpath\": \"/html/body/div\", \"regular_expression\": \"<<(.*):[0-9a-f]*:[0-9a-f]*:[0-9a-f]*:[0-9a-f]*:[0-9a-f]*:[0-9a-f]*>:[-]?[0-9][0-9]*>\", \"translator_class\": \"edu.uci.ics.luci.cacophony.node.TranslatorCategory\" } }\n\n\n'),
+	(4,'wifi_mac',X'000000000101000000B3F96EA939875DC0A41AD56DF9E04040',10,'{ \"do_training\": false, \"features\": [ { \"namespace\": null, \"names\": [ ] }, { \"namespace\": \"edu.uci.ics.luci.cacophony.milestone\", \"names\": [ \"wifi_ssid\" ] } ], \"target\": { \"format\": \"html\", \"url\": \"http://localhost:2011/sense?sensor=wifi&version=1.0&format=html\", \"xpath\": \"/html/body/div\", \"regular_expression\": \"<<.*:([0-9a-f]*:[0-9a-f]*:[0-9a-f]*:[0-9a-f]*:[0-9a-f]*:[0-9a-f]*)>:[-]?[0-9][0-9]*>\", \"translator_class\": \"edu.uci.ics.luci.cacophony.node.TranslatorCategory\" } }\n\n\n\n\n'),
+	(5,'volume',X'00000000010100000005C58F3177855EC01B9E5E29CBB04240',10,'{ \"do_training\": false, \"features\": [ { \"namespace\": null, \"names\": [ \"time\" ] }, { \"namespace\": \"edu.uci.ics.luci.cacophony.milestone\", \"names\": [ \"wifi_mac\" ] } ], \"target\": { \"format\": \"html\", \"url\": \"http://localhost:2011/sense?sensor=volume&version=1.0&format=html\", \"xpath\": \"/html/body/div\", \"regular_expression\": \"(.*)\", \"translator_class\": \"edu.uci.ics.luci.cacophony.node.TranslatorNumber\" } }\n\n\n\n\n'),
+	(6,'light',X'00000000010100000092EE42CE56935EC09AEB34D252274340',10,'{ \"do_training\": false, \"features\": [ { \"namespace\": null, \"names\": [ \"time\" ] }, { \"namespace\": \"edu.uci.ics.luci.cacophony.milestone\", \"names\": [ \"wifi_mac\" ] } ], \"target\": { \"format\": \"html\", \"url\": \"http://localhost:2011/sense?sensor=light&version=1.0&format=html\", \"xpath\": \"/html/body/div\", \"regular_expression\": \"(.*)\", \"translator_class\": \"edu.uci.ics.luci.cacophony.node.TranslatorNumber\" } }\n\n\n\n\n'),
+	(7,'ui_activity',X'000000000101000000248337FF8A895EC0DFB5749ABFB64240',10,'{ \"do_training\": false, \"features\": [ { \"namespace\": null, \"names\": [ \"time\" ] }, { \"namespace\": \"edu.uci.ics.luci.cacophony.milestone\", \"names\": [ \"wifi_mac\" ] } ], \"target\": { \"format\": \"html\", \"url\": \"http://localhost:2011/sense?uiactivity=light&version=1.0&format=html\", \"xpath\": \"/html/body/div\", \"regular_expression\": \"(.*)\", \"translator_class\": \"edu.uci.ics.luci.cacophony.node.TranslatorNumber\" } }\n\n\n\n\n'),
+	(8,'idle_time',X'000000000101000000B4AB90F293445DC0BBECD79DEE624040',10,'{ \"do_training\": false, \"features\": [ { \"namespace\": null, \"names\": [ ] }, { \"namespace\": \"edu.uci.ics.luci.cacophony.milestone\", \"names\": [ \"wifi_mac\" ] } ], \"target\": { \"format\": \"html\", \"url\": \"http://localhost:2011/sense?uiactivity=idle&version=1.0&format=html\", \"xpath\": \"/html/body/div\", \"regular_expression\": \"(.*)\", \"translator_class\": \"edu.uci.ics.luci.cacophony.node.TranslatorNumber\" } }\n\n\n\n\n'),
+	(9,'accelerometer_z',X'000000000101000000636CD73A279C5EC07A6B05F291E44240',10,'{ \"do_training\": false, \"features\": [ { \"namespace\": null, \"names\": [ ] }, { \"namespace\": \"edu.uci.ics.luci.cacophony.milestone\", \"names\": [ \"wifi_mac\" ] } ], \"target\": { \"format\": \"html\", \"url\": \"http://localhost:2011/sense?acclerometer=idle&version=1.0&format=html\", \"xpath\": \"/html/body/div\", \"regular_expression\": \",\\\\s*([0-9]*[.][0-9]*)[]]$\", \"translator_class\": \"edu.uci.ics.luci.cacophony.node.TranslatorNumber\" } }\n\n\n\n\n');
 
 /*!40000 ALTER TABLE `milestonetest` ENABLE KEYS */;
 UNLOCK TABLES;
