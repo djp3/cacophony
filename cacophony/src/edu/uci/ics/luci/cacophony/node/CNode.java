0b356ac0b1f5b1770d7895736f1122b2c18e8d64
Don Patterson <d_j_p_3@djp3.net>
2014-04-16 09:33:18 -0700

    Refactored Cnode so that I could write tests against it.  This mostly just affects the waiting time confidence interval stuff.

--- cacophony/src/edu/uci/ics/luci/cacophony/node/CNode.java
+++ cacophony/src/edu/uci/ics/luci/cacophony/node/CNode.java
@@ -1,7 +1,10 @@
 package edu.uci.ics.luci.cacophony.node;
 
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -81,31 +84,100 @@ public class CNode implements Runnable{
 		return null;
 	}
 	
-	private Long getNextUpdateTime(List<Long> durationTimes){
-		// use 95% confidence interval
-		final long ONE_SECOND = 1000;
-		final long DEFAULT_WAITING_TIME = 30 * ONE_SECOND;
 
-		if(durationTimes.isEmpty()){			
-			return (System.currentTimeMillis() + DEFAULT_WAITING_TIME);
+	protected static double calculateMean(List<Long> durationTimes) {
+		if((durationTimes == null) || (durationTimes.size() == 0)){
+			throw new InvalidParameterException("There needs to be samples");
 		}
 		
-		double mean = 0;
+		double mean = 0.0;
+		
 		for(int i=0; i<durationTimes.size(); i++){
 			mean += durationTimes.get(i);
 		}
 		mean = mean / durationTimes.size();
 		
+		return mean;
+	}
+	
+
+	protected static double calculateStdDev(List<Long> durationTimes, double mean) {
+		
+		if((durationTimes == null) || (durationTimes.size() == 0)){
+			throw new InvalidParameterException("There needs to be samples");
+		}
+		
 		double variance = 0;
-		for(int i=0; i<durationTimes.size(); i++){
+		for(int i=0; i < durationTimes.size(); i++){
 			variance += Math.pow((mean-durationTimes.get(i)),2);
 		}
 		variance = variance / durationTimes.size();
 		
-		double stdDev = Math.sqrt(variance);
-		double stdErrOfMean = stdDev / Math.sqrt(durationTimes.size());
-		long waitingTime = (long)(mean - 1.96*stdErrOfMean);
+		return Math.sqrt(variance);
+	}
+	
+	
+	protected static final Map<Double,Double> ztable = new HashMap<Double,Double>();
+	static {
+			ztable.put(Double.valueOf(0.50),Double.valueOf(0.50));
+			ztable.put(Double.valueOf(0.80),Double.valueOf(1.28));
+			ztable.put(Double.valueOf(0.90),Double.valueOf(1.64));
+			ztable.put(Double.valueOf(0.95),Double.valueOf(1.96));
+			ztable.put(Double.valueOf(0.99),Double.valueOf(2.58));
+	}
+	
+
+	protected final static long ONE_SECOND = 1000;
+	protected final static long DEFAULT_WAITING_TIME = 30 * ONE_SECOND;
+	
+	/**
+	 * 
+	 * See: http://en.wikipedia.org/wiki/Confidence_interval
+	 * @param durationTimes a list of samples
+	 * @param percentile 0 - 100
+	 * @return value above which percentile of the samples are expected to occur 
+	 */
+	protected static Double getPercentile(double mean, double stdDev, int n, Double percentile){
+		
+		if(n <= 0){
+			throw new InvalidParameterException("There needs to be samples");
+		}
+		
+		Double zTableLookup = ztable.get(percentile);
+		
+		if(zTableLookup == null){
+			throw new InvalidParameterException("We only support a few percentiles right now, and not: "+percentile);
+		}
+		
+		double stdErrOfMean = stdDev / Math.sqrt(n);
+		
+		return zTableLookup * stdErrOfMean;
+	}
+
+
+	protected static long getWaitingTime(List<Long> durationTimes) {
+		long waitingTime;
+		
+		try{
+			double mean = calculateMean(durationTimes);
+		
+			double stdDev = calculateStdDev(durationTimes, mean);
+		
+			Double confidenceInterval = Double.valueOf(0.95);
+		
+			double percentile = getPercentile(mean,stdDev,durationTimes.size(),confidenceInterval);
+			waitingTime = (long)(mean - percentile);
+		}
+		catch(InvalidParameterException e){
+			waitingTime = DEFAULT_WAITING_TIME;
+		}
+		return waitingTime;
+	}
+
+	public static Long getNextUpdateTime(List<Long> durationTimes){
+		long waitingTime = getWaitingTime(durationTimes);
 		
 		return System.currentTimeMillis() + waitingTime;
 	}
+
 }
--- cacophony/src/edu/uci/ics/luci/cacophony/node/CNodeTest.java
+++ cacophony/src/edu/uci/ics/luci/cacophony/node/CNodeTest.java
@@ -2,6 +2,10 @@ package edu.uci.ics.luci.cacophony.node;
 
 import static org.junit.Assert.*;
 
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
+import java.util.List;
+
 import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
 
@@ -15,6 +19,8 @@ import edu.uci.ics.luci.cacophony.server.CNodeServerTest;
 import edu.uci.ics.luci.cacophony.server.responder.ResponderConfigurationLoaderTest;
 
 public class CNodeTest {
+	
+	final double EPSILON = 0.0001;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -59,6 +65,185 @@ public class CNodeTest {
 		catch(RuntimeException e){
 			fail("Should not throw an exception:"+e);
 		}
+	}
+	
+	
+	@Test
+	public void testMean() {
+		
+		List<Long> durations = null;
+		try{
+			CNode.calculateMean(durations);
+			fail("This should not work");
+		}
+		catch(Exception e){
+			//ok
+		}
+		
+		durations = new ArrayList<Long>();
+		try{
+			CNode.calculateMean(durations);
+			fail("This should not work");
+		}
+		catch(Exception e){
+			//ok
+		}
+		
+		durations.add(10L);
+		assertTrue(Math.abs(10.0d - CNode.calculateMean(durations)) < EPSILON);
+		
+		durations.add(10L);
+		assertTrue(Math.abs(10.0d - CNode.calculateMean(durations)) < EPSILON);
+		
+		durations.add(10L);
+		assertTrue(Math.abs(10.0d - CNode.calculateMean(durations)) < EPSILON);
+		
+		durations.add(20L);
+		durations.add(20L);
+		durations.add(20L);
+		
+		assertTrue(Math.abs(15.0d - CNode.calculateMean(durations)) < EPSILON);
+		
+		durations.add(-15L);
+		durations.add(-15L);
+		durations.add(-15L);
+		durations.add(-15L);
+		durations.add(-15L);
+		durations.add(-15L);
+		
+		assertTrue(Math.abs(0.0d - CNode.calculateMean(durations)) < EPSILON);
+	}
+	
+	
+	
+	@Test
+	public void testStdDev() {
+		
+		List<Long> durations = null;
+		try{
+			CNode.calculateStdDev(durations,0.0);
+			fail("This should not work");
+		}
+		catch(Exception e){
+			//ok
+		}
+		
+		durations = new ArrayList<Long>();
+		try{
+			CNode.calculateStdDev(durations,0.0);
+			fail("This should not work");
+		}
+		catch(Exception e){
+			//ok
+		}
+		
+		durations.add(10L);
+		assertTrue(Math.abs(0.0d - CNode.calculateStdDev(durations,CNode.calculateMean(durations))) < EPSILON);
+		
+		durations.add(10L);
+		assertTrue(Math.abs(0.0d - CNode.calculateStdDev(durations,CNode.calculateMean(durations))) < EPSILON);
+		
+		durations.add(10L);
+		assertTrue(Math.abs(0.0d - CNode.calculateStdDev(durations,CNode.calculateMean(durations))) < EPSILON);
+		
+		durations.add(20L);
+		durations.add(20L);
+		durations.add(20L);
+		durations.add(20L);
+		durations.add(20L);
+		durations.add(20L);
+		
+		durations.add(20L);
+		durations.add(20L);
+		durations.add(20L);
+		durations.add(20L);
+		durations.add(20L);
+		durations.add(20L);
+		
+		assertTrue(Math.abs(4.0d - CNode.calculateStdDev(durations,CNode.calculateMean(durations))) < EPSILON);
+		
+	}
+	
+	
+	@Test
+	public void testPercentiles() {
+		double percentile;
+		try{
+			percentile = CNode.getPercentile(0,1.0, 1 ,0.0);
+			fail("This should fail on 0.0");
+		}
+		catch(InvalidParameterException e){
+			//ok
+		}
+		catch(Exception e){
+			fail("This should not happen:"+e);
+		}
+		
+		
+		Double confidenceInterval = Double.valueOf(0.95);
+		List<Long> durations = new ArrayList<Long>();
+		
+		try{
+			percentile = CNode.getPercentile(0,1.0, -1 ,confidenceInterval);
+			fail("This should fail on -1");
+		}
+		catch(InvalidParameterException e){
+			//ok
+		}
+		catch(Exception e){
+			fail("This should not happen:"+e);
+		}
+		
+		try{
+			percentile = CNode.getPercentile(0,1.0, 0 ,confidenceInterval);
+			fail("This should fail on 0");
+		}
+		catch(InvalidParameterException e){
+			//ok
+		}
+		catch(Exception e){
+			fail("This should not happen:"+e);
+		}
+		
+		assertTrue(Math.abs(1.96 - CNode.getPercentile(0,1.0, 1 ,confidenceInterval)) < EPSILON);
+		
+	}
+	
+	@Test
+	public void testWaitingTimes() {
+		List<Long> durations = new ArrayList<Long>();
+		
+		//This should "fail" because durations has no entries
+		assertEquals(CNode.DEFAULT_WAITING_TIME, CNode.getWaitingTime(durations));
+		
+		durations.add(10L);
+		
+		assertEquals(10L, CNode.getWaitingTime(durations));
+		
+		durations.add(10L);
+		durations.add(10L);
+		durations.add(10L);
+		durations.add(10L);
+		
+		assertEquals(10L, CNode.getWaitingTime(durations));
+		
+		durations = new ArrayList<Long>();
+		durations.add(10L);
+		durations.add(20L);
+		
+		assertEquals(8L, CNode.getWaitingTime(durations));
+	}
+	
+	@Test
+	public void testNextUpdateTimes() {
+		List<Long> durations = new ArrayList<Long>();
+		
+		durations.add(10000L);
+		durations.add(20000L);
+		
+		Long guess = System.currentTimeMillis()+8000;
+		Long toleranceInMS = 500L;
+		assertTrue(Math.abs(guess - CNode.getNextUpdateTime(durations)) < toleranceInMS);
 	}
 
 }
